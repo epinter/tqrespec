33e993e9d6a80def1174f0b9983b895adb26900f
Emerson Pinter <dev@pinter.com.br>
2018-10-27 03:14:23 +0000

    Remove test code from preloader

--- src/main/java/br/com/pinter/tqrespec/gui/AppPreloader.java
+++ src/main/java/br/com/pinter/tqrespec/gui/AppPreloader.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 public class AppPreloader extends Preloader {
     private ProgressBar bar;
     private Stage stage;
-    private boolean noLoadingProgress = true;
 
     private Scene createPreloaderScene() throws IOException {
         BorderPane pane = new BorderPane();
@@ -63,36 +62,9 @@ public class AppPreloader extends Preloader {
     }
 
     @Override
-    public void handleProgressNotification(ProgressNotification pn) {
-        //application loading progress is rescaled to be first 50%
-        //Even if there is nothing to load 0% and 100% events can be
-        // delivered
-        if (pn.getProgress() != 1.0 || !noLoadingProgress) {
-            bar.setProgress(pn.getProgress() / 2);
-            if (pn.getProgress() > 0) {
-                noLoadingProgress = false;
-            }
-        }
-    }
-
-    @Override
-    public void handleStateChangeNotification(StateChangeNotification evt) {
-        //ignore, hide after application signals it is ready
-    }
-
-    @Override
     public void handleApplicationNotification(PreloaderNotification pn) {
         if (pn instanceof ProgressNotification) {
-            //expect application to send us progress notifications
-            //with progress ranging from 0 to 1.0
-            double v = ((ProgressNotification) pn).getProgress();
-            if (!noLoadingProgress) {
-                //if we were receiving loading progress notifications
-                //then progress is already at 50%.
-                //Rescale application progress to start from 50%
-                v = 0.5 + v / 2;
-            }
-            bar.setProgress(v);
+            bar.setProgress(((ProgressNotification) pn).getProgress());
         } else if (pn instanceof StateChangeNotification) {
             //hide after get any state update from application
             stage.hide();
