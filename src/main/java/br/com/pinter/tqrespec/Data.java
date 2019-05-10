67ce104bf2c37d6f774d0858f6398bf2b35cef4c
Emerson Pinter <dev@pinter.com.br>
2019-05-10 13:21:09 +0000

    Add support to load database

---
+++ src/main/java/br/com/pinter/tqrespec/Data.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2018 Emerson Pinter - All Rights Reserved
+ */
+
+/*    This file is part of TQ Respec.
+
+    TQ Respec is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TQ Respec is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TQ Respec.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package br.com.pinter.tqrespec;
+
+import br.com.pinter.tqdatabase.Database;
+import br.com.pinter.tqdatabase.Text;
+
+import java.io.IOException;
+
+public class Data {
+    private static volatile Data _instance;
+    private Database db;
+    private Text text;
+
+    public static Data getInstance() {
+        Data d = _instance;
+        if (d == null) {
+            synchronized (Data.class) {
+                d = _instance;
+                if (d == null) {
+                    d = new Data();
+                    _instance = d;
+                }
+            }
+        }
+        return _instance;
+    }
+
+    private Data() {
+        String dbPath = null;
+        try {
+            dbPath = String.format("%s/Database/database.arz", GameInfo.getInstance().getGamePath());
+            db = new Database(dbPath);
+            text = new Text(GameInfo.getInstance().getGamePath() + "/Text");
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(String.format("Error loading database from '%s'", dbPath));
+        }
+    }
+
+    public Database getDb() {
+        return db;
+    }
+
+    public Text getText() {
+        return text;
+    }
+}
