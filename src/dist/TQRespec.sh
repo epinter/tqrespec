0077df0a53b299c114e3ae1c24e81e012627d73b
Emerson Pinter <e@pinter.dev>
2025-01-06 12:40:28 -0300

    Add LD_LIBRARY_PATH

--- src/dist/TQRespec.sh
+++ src/dist/TQRespec.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 MYDIR=$(realpath "${0%/*}")
+OLDLDLIB=""
+if [ -n "$LD_LIBRARY_PATH" ]; then
+    OLDLDLIB=":${LD_LIBRARY_PATH}"
+fi
+export LD_LIBRARY_PATH=${MYDIR}/lib/runtime/lib${OLDLDLIB}
 cd "$MYDIR"
 exec ./bin/TQRespec
