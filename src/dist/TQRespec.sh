342fb6a3c28c9d1ee815e2c39363b791877119ab
Emerson Pinter <e@pinter.dev>
2025-05-09 17:30:31 -0300

    Add script parameter to create shortcut on Linux

--- src/dist/TQRespec.sh
+++ src/dist/TQRespec.sh
@@ -6,4 +6,23 @@ if [ -n "$LD_LIBRARY_PATH" ]; then
 fi
 export LD_LIBRARY_PATH=${MYDIR}/lib/runtime/lib${OLDLDLIB}
 cd "$MYDIR"
-exec ./bin/TQRespec $@
+if [ "Xinstall" = "X$1" ]; then
+cat <<EOF > ~/.local/share/applications/TQRespec.desktop
+[Desktop Entry]
+Name=TQRespec
+Comment=The respec tool for Titan Quest
+GenericName=TQRespec
+Exec=${MYDIR}/TQRespec.sh
+Type=Application
+StartupNotify=true
+StartupWMClass=TQRespec
+Categories=Game;
+Keywords=TQRespec;
+Icon=${MYDIR}/lib/TQRespec.png
+Path=${MYDIR}
+EOF
+elif [ "Xuninstall" = "X$1" ]; then
+    exec rm -f ~/.local/share/applications/TQRespec.desktop
+else
+    exec ./bin/TQRespec $@
+fi
